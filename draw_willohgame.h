164a7140e4aa8f7bae6fadd2a22198761c56955b
Willoh Brubaker <willohbrubaker@outlook.com>
2024-11-17 18:59:51 -0600

    commit

--- draw_willohgame.h
+++ draw_willohgame.h
@@ -11,15 +11,15 @@
 void DrawWillohgame() {
     float x_pos = 0.2f;
     float y_pos = 1.2f;
-    float z_pos = 0.2f;
+    float z_pos = 1.2f;
 
     Color squareColor = PINK;
     Vector3 position = {
        14.0f,
-       0.9f,
+       20.9f,
        13.0f
        };
-       
+
 
     // Draw the square as a cube
     DrawCube(position, x_pos, y_pos, z_pos, squareColor); // Thin height for a flat square
