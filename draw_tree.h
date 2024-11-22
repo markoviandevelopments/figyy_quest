936891b63510883d47398bbc04f02f8105771376
Preston Brubaker <brubakerpreston@gmail.com>
2024-11-21 19:46:59 -0600

    tree color update!

--- draw_tree.h
+++ draw_tree.h
@@ -27,14 +27,14 @@ static inline void DrawLollipopTree(LollipopTree tree) {
     DrawCubeWires(stickPosition, tree.stickRadius, tree.stickHeight, tree.stickRadius, GRAY);       // Gray edges
 
     // Draw the lollipop head
-    Vector3 headPosition = { tree.position.x, tree.position.y + tree.stickHeight + tree.headRadius, tree.position.z };
+    Vector3 headPosition = { tree.position.x, tree.position.y + tree.stickHeight + tree.headRadius - 0.1f, tree.position.z };
     DrawSphere(headPosition, tree.headRadius, tree.headColor);
     DrawSphereWires(headPosition, tree.headRadius, 8, 16, GRAY);
 
     // Draw the lollipop ring
     float ringRadius = tree.headRadius * 1.1f;
     float ringHeight = 0.2f;
-    Vector3 ringPosition = { tree.position.x, tree.position.y + tree.stickHeight + tree.headRadius - 0.1f, tree.position.z };
+    Vector3 ringPosition = { tree.position.x, tree.position.y + tree.stickHeight + tree.headRadius - 0.2f, tree.position.z };
     DrawCylinder(ringPosition, ringRadius, ringRadius, ringHeight, 16, tree.headColor);
     DrawCylinderWires(ringPosition, ringRadius, ringRadius, ringHeight, 16, GRAY);
 }
@@ -48,37 +48,30 @@ static inline void DrawLollipopTrees(LollipopTree *trees, int treeCount) {
 
 // Custom list of lollipop colors with transparency (preconverted to raylib's Color format)
 const Color LOLLIPOP_COLOR_LIST[] = {
-    // Primary fruity flavors
-    (Color){255, 0, 0, 150},     // Red - Cherry
-    (Color){255, 69, 0, 150},    // Orange - Orange
-    (Color){255, 165, 0, 150},   // Bright Orange - Peach Mango
-    (Color){255, 255, 0, 150},   // Yellow - Lemon
-    (Color){0, 255, 0, 150},     // Green - Lime
-    (Color){0, 128, 0, 150},     // Dark Green - Green Apple
-    (Color){0, 255, 255, 150},   // Cyan - Blue Raspberry
-    (Color){0, 0, 255, 150},     // Blue - Grape
-    (Color){75, 0, 130, 150},    // Indigo - Blackberry
-    (Color){238, 130, 238, 150}, // Violet - Mixed Berry
-    (Color){255, 20, 147, 150},  // Deep Pink - Watermelon
-    (Color){255, 105, 180, 150}, // Hot Pink - Strawberry Shortcake
+    // Updated primary fruity flavors with brighter colors and varied transparency
+    (Color){255, 0, 0, 180},      // Bright Red - Cherry
+    (Color){255, 140, 0, 160},    // Brighter Orange - Tangerine
+    (Color){255, 215, 0, 170},    // Gold - Pineapple
+    (Color){255, 255, 0, 200},    // Lemon Yellow
+    (Color){0, 255, 0, 150},      // Lime Green
+    (Color){64, 224, 208, 190},   // Turquoise - Blue Raspberry
+    (Color){65, 105, 225, 160},   // Royal Blue - Blueberry
+    (Color){138, 43, 226, 185},   // Blue Violet - Grape
+    (Color){199, 21, 255, 175},   // Electric Purple - Wild Berry
+    (Color){255, 20, 147, 190},   // Vivid Pink - Watermelon
+    (Color){255, 105, 180, 195},  // Light Pink - Cotton Candy
 
-    // Creamy and dessert flavors
-    (Color){255, 222, 173, 150}, // Navajo White - Caramel
-    (Color){210, 105, 30, 150},  // Chocolate - Root Beer
+    // More exotic and fun mixes
+    (Color){255, 165, 0, 190},    // Orange - Mango Tango
+    (Color){173, 255, 47, 200},   // Green Yellow - Kiwi Lime
 
-    // Exotic and fun mixes
-    (Color){173, 216, 230, 150}, // Light Blue - Bubble Gum
-    (Color){255, 182, 193, 150}, // Light Pink - Pink Lemonade
-    (Color){255, 99, 71, 150},   // Tomato - Tropical Punch
-    (Color){255, 215, 0, 150},   // Gold - Pineapple
-    (Color){160, 82, 45, 150},   // Sienna - Chocolate Fudge
-
-    // Seasonal and limited edition
-    (Color){0, 128, 128, 150},   // Teal - Wintergreen
-    (Color){199, 21, 133, 150},  // Medium Violet Red - Raspberry Lemonade
-    (Color){255, 192, 203, 150}, // Pink - Peppermint Swirl
-    (Color){220, 20, 60, 150},   // Crimson - Pomegranate
+    // Expanded seasonal and limited edition
+    (Color){255, 69, 0, 160},     // Orange Red - Spiced Pumpkin
+    (Color){255, 215, 0, 175},    // Goldenrod - Honey Gold
+    (Color){218, 112, 214, 130},  // Orchid - Spring Lavender
+    (Color){219, 112, 147, 140},  // Pale Violet Red - Cherry Blossom
 };
+
 
 
 const int LOLLIPOP_COLOR_COUNT = sizeof(LOLLIPOP_COLOR_LIST) / sizeof(LOLLIPOP_COLOR_LIST[0]);
